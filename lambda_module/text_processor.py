37e1d9f42560af31dd5a207d04db41ad6b876146
HirotoOsaka <ipocchi129@gmail.com>
2024-11-17 16:10:12 +0900

    add: llm.py

--- lambda_module/text_processor.py
+++ lambda_module/text_processor.py
@@ -1,8 +1,4 @@
-import json
-import os
-from abc import abstractmethod
-
-from openai import OpenAI
+from llm import AbstractLLM
 
 
 class Questions:
@@ -20,53 +16,6 @@ class Questions:
 
     def to_question_str(self, question: str) -> str:
         return self.question_dict.get(question, "No Question")
-
-
-class AbstractLLM:
-    def __init__(self):
-        self.client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
-
-    def dict2json(self, python_dict):
-        json_string = json.dumps(python_dict, indent=2, ensure_ascii=False)
-        return json_string
-
-    def json2dict(self, json_string, error_key="error") -> dict:
-        try:
-            python_dict = json.loads(
-                self._extract_string(json_string, start_string="{", end_string="}"),
-                strict=False,
-            )
-        except ValueError:
-            if error_key is None:
-                return json_string
-            python_dict = {error_key: json_string}
-        if isinstance(python_dict, dict):
-            return python_dict
-        return {error_key: python_dict}
-
-    def _extract_string(self, text, start_string=None, end_string=None) -> str:
-        # 最初の文字
-        if start_string is not None and start_string in text:
-            idx_head = text.index(start_string)
-            text = text[idx_head:]
-        # 最後の文字
-        if end_string is not None and end_string in text:
-            idx_tail = len(text) - text[::-1].index(end_string[::-1])
-            text = text[:idx_tail]
-        return text
-
-    @abstractmethod
-    def preprocess(self, text: str) -> list[dict]:
-        pass
-
-    def generate_completion(self, text: str, **kwargs) -> str:
-        messages = self.preprocess(text)
-        response = self.client.chat.completions.create(messages=messages, **kwargs)
-        return self.postprocess(response.choices[0].message.content)
-
-    @abstractmethod
-    def postprocess(self, text: str):
-        pass
 
 
 class TitleExtractor(AbstractLLM):
@@ -94,7 +43,8 @@ class TitleExtractor(AbstractLLM):
 class ContentSummarizer(AbstractLLM):
     def preprocess(self, text, questions):
         output_format = {
-            question: f"(string) Answer to {question} in markdown format" for question in questions
+            question: f"(string) Answer to {question} in markdown format"
+            for question in questions
         }
         system_prompt = (
             "与えられる文章を読み込み、以下の問いに答えて下さい。\n"
